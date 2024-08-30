f21b34588a96f5627d7928cf897eabb4de101938
Shyim <s.sayakci@gmail.com>
2024-08-30 14:42:03 +0200

    Update app-url.sh

--- rootfs/etc/scripts/app-url.sh
+++ rootfs/etc/scripts/app-url.sh
@@ -4,6 +4,7 @@ until mariadb-admin ping; do sleep 1; done
 
 if [[ -n $APP_URL ]]; then
   mariadb -uroot -proot shopware -e "UPDATE sales_channel_domain set url = '${APP_URL}'"
+  rm -rf /var/www/html/var/cache
 fi
 
 echo "Updated sales_channel_domain, sleeping now..."
