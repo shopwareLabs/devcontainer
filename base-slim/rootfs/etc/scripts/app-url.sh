71f30ac4549becb768d7e29e27581916963be4bc
Shyim <s.sayakci@gmail.com>
2024-11-11 15:44:46 +0100

    Update app-url.sh

--- base-slim/rootfs/etc/scripts/app-url.sh
+++ base-slim/rootfs/etc/scripts/app-url.sh
@@ -2,12 +2,12 @@
 
 wait-for-mysql
 
-if [[ -n $CODESPACE_NAME ]]; then
+if [ -n "$CODESPACE_NAME" ]; then
   echo "Detected GitHub Codespace"
   export APP_URL="https://${CODESPACE_NAME}-8000.app.github.dev"
 fi
 
-if [[ -n $APP_URL ]]; then
+if [ -n "$APP_URL" ]; then
   mariadb -uroot -proot shopware -e "UPDATE sales_channel_domain set url = '${APP_URL}'"
   rm -rf /var/www/html/var/cache
 fi
